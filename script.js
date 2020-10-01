45f1ef6553f75574db538f50a6c9272671234580
bryan <bdvegat@unal.edu.co>
2020-09-30 22:40:53 -0500

    second commit

---
+++ script.js
@@ -0,0 +1,47 @@
+function pestel_p(){
+    document.getElementById("d").innerHTML =
+    "Plan de desarrollo distrital de la Alcaldía Local de Bogotá 2020- 2024: Educación cierre de brechas, acceso y permanencia con equidad (detorrealbaf, 2020)"
+;
+}
+
+function pestel_e(){
+    document.getElementById("d").innerHTML =
+    "Inversión al sector educativo por parte del distrito: $23.631.915 serán destinados al sector educativo, el cierre de brechas y permanencia con equidad (detorrealbaf, 2020)"
+;
+}
+
+function pestel_s(){
+    document.getElementById("d").innerHTML =
+    "Desigualdad en el acceso educación de calidad: De los mejores colegios en los resultados para las pruebas SABER 11 del año 2019 ninguno de los 20 primeros clasificados es colegio público en la ciudad de Bogotá (Revista Dinero, 2019)"
+;
+}
+
+function pestel_t(){
+    document.getElementById("d").innerHTML =
+    "Conectividad y accesos a internet: De los 6218 colegios registrados en la ciudad de Bogotá para el año 2019 un 26% tiene acceso a conexión de internet durante todos los días de la semana, 69% al menos una vez a la semana, el 2% lo usa al menos una vez al mes, menos del 1% lo usa al menos una vez al mes pero no todos los días del año y un 2,1% no lo usa ningún día de la semana. (DANE, 2019)"
+;
+}
+
+function pestel_e2(){
+    document.getElementById("d").innerHTML =
+    "Política RESPEL: Política ambiental para la gestión integral de residuos peligrosos y los riesgos que esto conlleva (Ministerio del Medio Ambiente, 2016)"
+;
+}
+
+function pestel_l(){
+    document.getElementById("d").innerHTML =
+    "Leyes a favor de las TIC's y los emprendimientos: Es de la normatividad general, empresas nuevas o viejas, grandes o pequeñas, que inviertan en proyectos calificados por el consejo Nacional de Beneficios Tributarios en Ciencia y Tecnología en Innovación como de investigación, desarrollo tecnológico o innovación, tendrán derecho a descontar de su impuesto sobre la renta a cargo, el 25% del valor invertido en dichos proyectos en el período gravable en que se realizó la inversión.La propuesta debe contar con el aval de una entidad u organización reconocida por Colciencias."
+;
+}
+
+function ods1(){
+    document.getElementById("d").innerHTML =
+    "Asegurar para 2030 que todos los niños y niñas terminen la educación básica y secundaria."
+;
+}
+
+function ods2(){
+    document.getElementById("d").innerHTML =
+    "De aquí a 2030, potenciar y promover la inclusión social, económica y política de todas las personas."
+;
+}
